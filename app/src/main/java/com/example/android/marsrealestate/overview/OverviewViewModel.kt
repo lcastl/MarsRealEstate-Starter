a16ae564b3ed4ea9fbd0ced5a92aa482509db614
lcastellanos <lcastellanosa@hotmail.com>
2021-09-22 02:04:51 -0500

    [Add] parse the json response with moshi

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,9 +21,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -49,12 +51,13 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsRealEstateProperties() {
         MarsApi.retrofitService.getProperties().enqueue(
-            object: Callback<String> {
-                override fun onFailure(call: Call<String>, t: Throwable) {
+            object: Callback<List<MarsProperty>> {
+                override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
                     _response.value = "Failure: " + t.message
                 }
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    _response.value = response.body()
+                override fun onResponse(call: Call<List<MarsProperty>>,
+                                        response: Response<List<MarsProperty>>) {
+                    _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
                 }
 
             })
